80240525cec651cfa0e98f646b15d4a586dd66d7
Bekzat <bek20001201@gmail.com>
2023-10-20 04:48:30 +0500

    nm
    nm

---
+++ BI.API/Controllers/ProjectController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using BI.Domain.DTOs;
+using BI.Domain.Exceptions;
+using BI.Domain.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BI.API
+{
+    [Route("api/projects")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProjects()
+        {
+            try
+            {
+                var projects = await _projectService.GetProjects();
+                return Ok(projects);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProject(int id)
+        {
+            try
+            {
+                var project = await _projectService.GetProject(id);
+                return Ok(project);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProject([FromBody] CreateProjectDTO createProjectDTO)
+        {
+            try
+            {
+                var project = await _projectService.CreateProject(createProjectDTO);
+                
+                return CreatedAtAction("GetProject", new { id = project.Id }, project);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProject(int id, [FromBody] UpdateProjectDTO updateProjectDTO)
+        {
+            try
+            {
+                var result = await _projectService.UpdateProject(id, updateProjectDTO);
+                if (result)
+                {
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            try
+            {
+                var result = await _projectService.DeleteProject(id);
+                
+                if (result)
+                {
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+    }
+}
