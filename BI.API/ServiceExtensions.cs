40820e441a86c011448d384be12c58e4f05e8a08
Bekzat <bek20001201@gmail.com>
2023-10-20 05:22:43 +0500

    Update ServiceExtensions.cs

--- BI.API/ServiceExtensions.cs
+++ BI.API/ServiceExtensions.cs
@@ -63,9 +63,7 @@ namespace BI.API
             {
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Description = @"JWT Authorization header using the Bearer scheme. 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      Example: 'Bearer 12345abcdef'",
+                    Description = @"Пример: Bearer 12345abcdefasdgadsfdasfdsafdsfasdsfafdsfafsdfafasd",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
